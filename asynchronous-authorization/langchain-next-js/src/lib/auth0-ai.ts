3d7e0bc29bf68eaf045cca5b7e1b51ceba210774
Patrick M. Riley <patrickmriley@proton.me>
2025-09-22 18:33:31 -0400

    feat: async auth updates

--- asynchronous-authorization/langchain-next-js/src/lib/auth0-ai.ts
+++ asynchronous-authorization/langchain-next-js/src/lib/auth0-ai.ts
@@ -13,6 +13,13 @@ export const withAsyncAuthorization = auth0AI.withAsyncUserConfirmation({
   scopes: ['openid', 'product:buy'], // add any scopes you want to use with your API
   audience: process.env['SHOP_API_AUDIENCE']!,
 
+
+  /**
+   * Note: setting a requestedExpiry to >= 301 will currently ensure email is used. Otherwise,
+   * the default is to use push notification if available.
+  */
+  // requestedExpiry: 301,
+
   /**
    * When this flag is set to `block`, the execution of the tool awaits
    * until the user approves or rejects the request.
@@ -23,7 +30,11 @@ export const withAsyncAuthorization = auth0AI.withAsyncUserConfirmation({
    * In practice, the process that is awaiting the user confirmation
    * could crash or timeout before the user approves the request.
    */
-  onAuthorizationRequest: 'block',
+  onAuthorizationRequest: async (authReq, creds) => {
+    console.log(`An authorization request was sent to your mobile device or your email.`);
+    await creds;
+    console.log(`Thanks for approving the order.`);
+  },
   onUnauthorized: async (e: Error) => {
     if (e instanceof AccessDeniedInterrupt) {
       return 'The user has denied the request';
