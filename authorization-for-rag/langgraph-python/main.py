a2807a1966a266c975971a9bc96a46b98ce22c71
Patrick M. Riley <patrickmriley@proton.me>
2025-09-30 15:26:46 -0400

    feat: authorization-for-rag/langgraph-python updates

--- authorization-for-rag/langgraph-python/main.py
+++ authorization-for-rag/langgraph-python/main.py
@@ -7,7 +7,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph
 from openfga_sdk.client.models import ClientBatchCheckItem
-from langchain_auth0_ai import FGARetriever
+from auth0_ai_langchain import FGARetriever
 
 from helpers.memory_store import MemoryStore
 from helpers.read_documents import read_documents
