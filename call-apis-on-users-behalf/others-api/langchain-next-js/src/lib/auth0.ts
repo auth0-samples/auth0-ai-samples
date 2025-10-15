15c7ec6ee760d220547a35762c968c36a9536b0f
Adam Woźniewski <adam.baaka@gmail.com>
2025-10-15 15:10:55 +0200

    AIDX-237 Add new calls to sample apps

--- call-apis-on-users-behalf/others-api/langchain-next-js/src/lib/auth0.ts
+++ call-apis-on-users-behalf/others-api/langchain-next-js/src/lib/auth0.ts
@@ -4,6 +4,7 @@ export const auth0 = new Auth0Client({
   authorizationParameters: {
     scope: process.env.AUTH0_SCOPE,
     audience: process.env.AUTH0_AUDIENCE,
+    enableConnectAccountEndpoint: true
   },
 });
 
